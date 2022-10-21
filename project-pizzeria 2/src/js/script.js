55ebdff4e8c88f7dbfc3c7bdb08a5ea81219ed86
Michal Frankiewicz <michal_frankiewicz@outlook.com>
2022-10-21 11:06:59 +0200

    Add setValue,  initActions, announce, (bad price)

--- project-pizzeria 2/src/js/script.js
+++ project-pizzeria 2/src/js/script.js
@@ -43,7 +43,7 @@
     const settings = {
         amountWidget: {
             defaultValue: 1,
-            defaultMin: 1,
+            defaultMin: 0,
             defaultMax: 9,
         }
     };
@@ -61,6 +61,7 @@
             thisProduct.getElements();
             thisProduct.initAccordion();
             thisProduct.initOrderForm();
+            thisProduct.initAmountWidget();
             thisProduct.processOrder();
             console.log('new Product: ', thisProduct);
             0;
@@ -87,6 +88,7 @@
             thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
             thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
             thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+            thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
         }
 
         initAccordion() {
@@ -173,11 +175,79 @@
                     }
 
                     // update calculated price in the HTML
+                    /* multiply price by amount */
+                    price *= thisProduct.amountWidget.value;
                     thisProduct.priceSingle = price;
                     thisProduct.priceElem.innerHTML = price;
                     console.log(optionId, option);
                 }
             }
+
+
+        }
+        initAmountWidget() {
+            const thisProduct = this;
+            thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+            thisProduct.amountWidgetElem.addEventListener('updated', function() {
+
+                thisProduct.processOrder();
+            });
+        }
+    }
+
+    class AmountWidget {
+        constructor(element) {
+            const thisWidget = this;
+            thisWidget.getElements(element);
+            thisWidget.setValue(settings.amountWidget.defaultValue);
+            thisWidget.setValue(thisWidget.input.value || settings.amountWidget.defaultValue);
+            thisWidget.initActions();
+            console.log('AmountWidget: ', thisWidget);
+            console.log('constructor arguments: ', element);
+
+        }
+        getElements(element) {
+            const thisWidget = this;
+
+            thisWidget.element = element;
+            thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+            thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+            thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+        }
+
+        setValue(value) {
+            const thisWidget = this;
+            const newValue = parseInt(value);
+
+            /* TODO: Add validation */
+            if (thisWidget.value !== newValue && !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) {
+                thisWidget.value = newValue;
+            }
+
+            thisWidget.input.value = thisWidget.value;
+            thisWidget.announce();
+
+        }
+        announce() {
+            const thisWidget = this;
+            const event = new CustomEvent('updated', {
+                bubbles: true
+            });
+            thisWidget.element.dispatchEvent(event);
+        }
+        initActions() {
+            const thisWidget = this;
+            thisWidget.input.addEventListener('change', function() {
+                thisWidget.setValue(thisWidget.input.value);
+            });
+            thisWidget.linkDecrease.addEventListener('click', function(event) {
+                event.preventDefault();
+                thisWidget.setValue((thisWidget.value - 1));
+            });
+            thisWidget.linkIncrease.addEventListener('click', function(event) {
+                event.preventDefault();
+                thisWidget.setValue((thisWidget.value + 1));
+            });
 
         }
     }
